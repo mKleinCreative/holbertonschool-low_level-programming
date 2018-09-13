3e055e0baffdbfd557142e0df85c623784ac62e7
mKleinCreative <miketheklein@gmail.com>
2018-09-13 19:33:41 +0000

    pushing up 3 for testing

--- 0x01-variables_if_else_while/3-print_alphabt.c
+++ 0x01-variables_if_else_while/3-print_alphabt.c
@@ -1,13 +1,23 @@
-<#include stdio>
+#include <stdio.h>
 
 /**
  * main - Entry point
  *
- * Return (Success)
+ * Return: prints the alphabet without q or e (Success)
  */
 
 int main(void)
 {
+	char ch;
+
+	for (ch = 'a'; ch < 'z'; ch++)
+	{
+		if (ch == 'e' || ch == 'q')
+			continue;
+		putchar(ch);
+	}
+	putchar('\n');
+
 	return (0);
 }
 

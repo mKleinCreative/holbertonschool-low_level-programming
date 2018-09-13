7f4f0fa3a90283101f36c91d19d21ac5317c635e
mKleinCreative <miketheklein@gmail.com>
2018-09-13 20:05:45 +0000

    pushing printing numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,13 +1,20 @@
-<#include stdio>
+#include <stdio.h>
 
 /**
  * main - Entry point
  *
- * Return (Success)
+ * Return: (Success)
  */
 
 int main(void)
 {
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+		putchar(n);
+
+	putchar('\n');
+
 	return (0);
 }
 

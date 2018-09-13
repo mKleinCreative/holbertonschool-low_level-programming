ce8b4663c706610872642d118c024e00f4e0e1a3
mKleinCreative <miketheklein@gmail.com>
2018-09-13 20:21:05 +0000

    pushing base 16

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,13 +1,18 @@
-<#include stdio>
+#include <stdio.h>
 
 /**
  * main - Entry point
  *
- * Return (Success)
+ * Return: (Success)
  */
 
 int main(void)
 {
+	char ch;
+
+	for (ch = 'z'; ch >= 'a'; ch--)
+		putchar(ch);
+	putchar('\n');
+
 	return (0);
 }
-
--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,13 +1,21 @@
-<#include stdio>
+#include <stdio.h>
 
 /**
  * main - Entry point
  *
- * Return (Success)
+ * Return: (Success)
  */
 
 int main(void)
 {
+	int n;
+	char ch;
+	for (n = '0'; n <= '9'; n++)
+		putchar(n);
+	for (ch = 'a'; ch <= 'f'; ch++)
+		putchar(ch);
+	putchar('\n');
+
 	return (0);
 }
 

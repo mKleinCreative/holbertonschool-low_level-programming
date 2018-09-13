dd55916c4a987efeadfe084a6d24f9920872bd63
mKleinCreative <miketheklein@gmail.com>
2018-09-13 20:53:09 +0000

    pushing print comb for testing

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,13 +1,25 @@
-<#include stdio>
+#include <stdio.h>
 
 /**
  * main - Entry point
  *
- * Return (Success)
+ * Return: (Success)
  */
 
 int main(void)
 {
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(n);
+		if (n != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
 	return (0);
 }
 

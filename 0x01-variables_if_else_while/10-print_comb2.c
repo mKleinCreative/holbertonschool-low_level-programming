973a7029a8aa8b4f416966a99eb924becc1bf5a2
mKleinCreative <miketheklein@gmail.com>
2018-09-13 21:06:17 +0000

    pushing 10

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -1,13 +1,26 @@
-<#include stdio>
+#include <stdio.h>
 
 /**
  * main - Entry point
  *
- * Return (Success)
+ * Return: (Success)
  */
 
 int main(void)
 {
+	int n;
+
+	for (n = 0; n < 100; n++)
+	{
+		putchar((n/10)+'0');
+		putchar((n%10)+'0');
+		if (n != 99)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
 	return (0);
 }
 

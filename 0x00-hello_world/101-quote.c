8bcd466ffe62fcb79cb448b1ca6b32570ef0da33
mKleinCreative <miketheklein@gmail.com>
2018-09-14 20:21:25 +0000

    completed 101 0x01

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+/**
+ * main - Entry point
+ *
+ * Return:  (Success)
+ */
+
+int main(void)
+{
+	write(0, "\"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (0);
+}

a65cfc8c505ffc4cf95c1dea04e165c30397d9dc
mKleinCreative <miketheklein@gmail.com>
2018-09-18 18:37:47 +0000

    pushing testing corrections for 7 and 6

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,10 +1,11 @@
 /**
- * main - Entry point
- *
- * Return: (Success)
+ * _abs - Entry point
+ * returns the absolute value of n.
+ * @n: an integer to convert into an absolute number.
+ * Return: the absolute value of n (Success)
  */
 
 int _abs(int n)
 {
-	return n * ((n>0) - (n<0));
+	return (n * ((n > 0) - (n < 0)));
 }

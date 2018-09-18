428554110d513e3c8a0460be8eff0b568cf473da
mKleinCreative <miketheklein@gmail.com>
2018-09-18 20:57:26 +0000

    pushing 100 times table up for testing after local success

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * print_times_table - Entry point
+ * @n: prints an n sized times table starting with 0.
+ *
+ * Return: The sum given the two arguements. (Success)
+ */
+
+void print_times_table(int n)
+{
+	int rows;
+	int columns;
+
+	if (n > 15 || n < 0)
+		return;
+	for (rows = 0; rows <= n; rows++)
+	{
+		for (columns = 0; columns <= n; columns++)
+		{
+			format_product(rows, columns, n);
+		}
+	}
+}
+
+
+/**
+ * format_product - Utility
+ * @row: current row of times table.
+ * @col: current column of times table.
+ * @total: what the times table is heading towards.
+ * Return: The sum given the two arguements. (Success)
+ */
+
+void format_product(int row, int col, int total)
+{
+	int product;
+
+	product = row * col;
+	if (col == total)
+	{
+		num_of_spaces(product);
+		printf("%d\n", product);
+	} else
+	{
+		num_of_spaces(product);
+		printf("%d,", product);
+	}
+}
+
+
+/**
+ * num_of_spaces - Utility
+ * @num: prints the number of spaces required to keep everything inline.
+ * provides 1 space for single digit numbers,
+ * 2 for double and 3 for triple digit numbers.
+ *
+ * Return: The sum given the two arguements. (Success)
+ */
+
+void num_of_spaces(int num)
+{
+	if (num >= 100)
+	{
+		printf(" ");
+	} else if (num <= 99 && num >= 10)
+	{
+		printf("  ");
+	} else
+	{
+		printf("   ");
+	}
+}

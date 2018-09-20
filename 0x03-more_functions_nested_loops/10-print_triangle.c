74d1190e3a0119952ce745588912950e9e5d3f88
mKleinCreative <miketheklein@gmail.com>
2018-09-20 20:33:00 +0000

    pushing print triangle for testing

--- 0x03-more_functions_nested_loops/10-print_triangle.c
+++ 0x03-more_functions_nested_loops/10-print_triangle.c
@@ -1,14 +1,29 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "holberton.h"
 
 /**
- * main - Entry point
- *
+ * print_triangle - prints a triangle followed by a new line
+ * @size: size of the triangle
  * Return: (Success)
  */
 
-int main(void)
+void print_triangle(int size)
 {
-	return (0);
+	int spaces, hashes, row;
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+	for (row = 0; row <= size - 1; row++)
+	{
+		for (spaces = 0; spaces < (size - 1) - row; spaces++)
+		{
+			_putchar(' ');
+		}
+		for (hashes = 0; hashes <= row; hashes++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
 }
-

228058b8c0c170c6c98c94a91d43e02e8003aeac
mKleinCreative <miketheklein@gmail.com>
2018-09-25 20:23:09 +0000

    fixed logic and betty errors for 8

--- 0x04-pointers_arrays_strings/8-print_array.c
+++ 0x04-pointers_arrays_strings/8-print_array.c
@@ -13,12 +13,17 @@ void print_array(int *a, int n)
 
 	for (i = 0; i < n; i++)
 	{
-		if (a[i] == 0)
-		       printf("%d,", a[i]);
-		else if (i == n - 1)
+		if (i == 0)
+		{
+			printf("%d,", a[i]);
+		} else if (i == n - 1)
+		{
 			printf(" %d", a[i]);
-		else
+		} else
+		{
 			printf(" %d,", a[i]);
+		}
 	}
+	printf("\n");
 }
 

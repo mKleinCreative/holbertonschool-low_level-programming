52db013a3e570a89f1fbefc43e8ea41487664429
mKleinCreative <miketheklein@gmail.com>
2018-09-25 22:03:21 +0000

    refactored atoi and pushing to test edge cases

--- 0x04-pointers_arrays_strings/100-atoi.c
+++ 0x04-pointers_arrays_strings/100-atoi.c
@@ -7,24 +7,19 @@
  */
 int _atoi(char *s)
 {
-	int i, j, neg;
+	int i, neg;
 	int total;
 
 	total = 0;
 	neg = 1;
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (s[i] == 59)
+		if (total > 0 && (s[i] > 57 || s[i] < 48))
 			break;
+		if (s[i] == 45)
+			neg = neg * -1;
 		if (s[i] <= 57 && s[i] >= 48)
 			total = total * 10 + s[i] - 48;
-	}
-	for (j = 0; s[j] != '\0'; j++)
-	{
-		if (s[i] == 59)
-			break;
-		if (s[j] == 45)
-			neg = neg * -1;
 	}
 	if (neg < 0)
 		return (total * -1);

490fe3e87563250b2441c2b79ef3c14e5c463d8b
mKleinCreative <miketheklein@gmail.com>
2018-09-25 19:57:27 +0000

    simplified code in 6 to one line, hopefully should work now

--- 0x04-pointers_arrays_strings/6-puts2.c
+++ 0x04-pointers_arrays_strings/6-puts2.c
@@ -8,18 +8,14 @@
 
 void puts2(char *str)
 {
-	int i, length;
+	int i;
 
-	length = 0;
 	for (i = 0; str[i] != '\0'; i++)
-	{
-		length++;
-	}
-	for (i = 0; i < length; i++)
 	{
 		if (!(i % 2))
 			_putchar(str[i]);
 		else
 			continue;
 	}
+	_putchar('\n');
 }

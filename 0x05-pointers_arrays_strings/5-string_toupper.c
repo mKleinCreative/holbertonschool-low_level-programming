c23a134e584c15d332f17d4fb2056519562ad55d
mKleinCreative <miketheklein@gmail.com>
2018-09-28 01:54:29 +0000

    pushing minor bug fixes for 1 and testing 5

--- 0x05-pointers_arrays_strings/5-string_toupper.c
+++ 0x05-pointers_arrays_strings/5-string_toupper.c
@@ -3,8 +3,14 @@
  * @*: char to uppercase
  * Return: (Success)
  */
-char *string_toupper(char *)
+char *string_toupper(char *ch)
 {
-	return (0);
-}
+	int i;
 
+	for (i = 0; ch[i] != '\0'; i++)
+	{
+		if (ch[i] >= 97 && ch[i] <= 122)
+			ch[i] = ch[i] - 32;
+	}
+	return (ch);
+}

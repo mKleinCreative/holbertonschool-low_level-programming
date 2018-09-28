59d7739766b146d9f570e0ec3cdf077f25a2d9c8
mKleinCreative <miketheklein@gmail.com>
2018-09-28 06:07:15 +0000

    testing 6

--- 0x05-pointers_arrays_strings/6-cap_string.c
+++ 0x05-pointers_arrays_strings/6-cap_string.c
@@ -1,14 +1,61 @@
-#include <stdio.h>
-#include <stdlib.h>
-
 /**
- * main - Entry point
- *
- * Return: (Success)
+ * is_separator - checks to see if character is a separator of words
+ * @ch: character to check
+ * Return: 0 if false, 1 if true
  */
 
-int main(void)
+int is_separator(char ch)
 {
-	return (0);
+	int ans;
+
+	ans = 0;
+	if (ch == ' ' || ch == ',' || ch == ';' || ch == '.' || ch == '!' || ch == '?')
+	{
+		ans = 1;
+		return (ans);
+	} else if (ch == '\t' || ch == '\n')
+	{
+		ch = ' ';
+		ans = 1;
+		return (ans);
+	} else if (ch == '"' || ch == '(' || ch == ')' || ch == '{' || ch == '}')
+	{
+		ans = 1;
+		return (ans);
+	}
+	return (ans);
+}
+
+
+/**
+ * cap_string - capitalizes all words of a string.
+ * @str: string sentence to run command over
+ * Return: modified string with capital letters
+ */
+
+char *cap_string(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i == 0)
+		{
+			if ((str[i] >= 'a' && str[i] <= 'z'))
+				str[i] = str[i] - 32;
+			continue;
+		}
+		if (is_separator(str[i]))
+		{
+			++i;
+
+			if ((str[i] >= 'a' && str[i] <= 'z'))
+			{
+				str[i] = str[i] - 32;
+				continue;
+			}
+		}
+	}
+	return (str);
 }
 

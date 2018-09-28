65644176b8847324f8cb717b187573590ecefd94
mKleinCreative <miketheklein@gmail.com>
2018-09-28 20:05:01 +0000

    checking 100 print number

--- 0x05-pointers_arrays_strings/100-print_number.c
+++ 0x05-pointers_arrays_strings/100-print_number.c
@@ -5,19 +5,22 @@
  */
 void print_number(int n)
 {
+	unsigned int num;
+
 	if (n < 0)
 	{
 		_putchar('-');
-		n = n * -1;
+		num = n  * -1;
+	} else {
+		num = n;
 	}
-	if (n < 10)
+	if (num  < 10)
 	{
-		_putchar((n % 10) + '0');
+		_putchar((num  % 10) + '0');
 		return;
-	}
-	else
+	} else
 	{
-		print_number(n / 10);
+		print_number(num  / 10);
 	}
-	_putchar((n % 10) + '0');
+	_putchar((num  % 10) + '0');
 }

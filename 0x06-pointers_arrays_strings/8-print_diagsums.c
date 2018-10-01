bf1f1c050986660519279c9d302ca52cb7d521bb
mKleinCreative <miketheklein@gmail.com>
2018-10-01 23:17:35 +0000

    pushing up wip of 8

--- 0x06-pointers_arrays_strings/8-print_diagsums.c
+++ 0x06-pointers_arrays_strings/8-print_diagsums.c
@@ -1,13 +1,32 @@
+#include <stdio.h>
+
 /**
- *  - 
- * @s: memory area pointed to.
- * @c: constant byte to fill memory with.
- @ @n: size of memory area to fill
- * Return: pointer to s
+ * print_diagsums - prints the sum of two diagonals of a square matrix
+ * @a: memory area pointed to.
+ * @size: constant byte to fill memory with.
+ * Return: sums
  */
 
-void *()
+void print_diagsums(int *a, int size)
 {
-	
-}
+	int i, j;
+	unsigned int sum1, sum2;
 
+	sum1 = sum2 = 0;
+	for (i = 0; i < (size * size); i++)
+	{
+		if ((i % size) == 0)
+		{
+			++i;
+			sum1 = sum1 + *(a + i);
+		}
+	}
+	for (j = size * size; j > 0; j--)
+	{
+		if ((i % (size - 1) == 0))
+			sum2 = sum2 + *(a + j);
+	}
+
+	printf("%d, ", sum1);
+	printf("%d\n", sum2);
+}

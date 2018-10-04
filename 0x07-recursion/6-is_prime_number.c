32a920c4b8be9493deb5da033846d982cf4fd7f9
mKleinCreative <miketheklein@gmail.com>
2018-10-04 01:33:11 +0000

    pushing up 6 for testing

--- 0x07-recursion/6-is_prime_number.c
+++ 0x07-recursion/6-is_prime_number.c
@@ -1,14 +1,24 @@
-#include <stdio.h>
-#include <stdlib.h>
 
+int prime_check(int n, int i)
+{
+	if (i == 1)
+		return (1);
+	else if (n % i == 0)
+		return (0);
+	else if (n % i != 0)
+		return (prime_check(n, i - 1));
+	return (0);
+}
 /**
- * main - Entry point
- *
+ * is_prime_number - returns 1 if the input is prime
+ * @n: number to find if its prime number.
  * Return: (Success)
  */
 
-int main(void)
+int is_prime_number(int n)
 {
-	return (0);
+	if (n <= 1)
+		return (0);
+	return (prime_check(n, n - 1));
 }
 

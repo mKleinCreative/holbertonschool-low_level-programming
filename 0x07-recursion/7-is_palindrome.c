31d5bf5c4a5a7af017e47e416186ad1db76a614a
mKleinCreative <miketheklein@gmail.com>
2018-10-04 02:21:08 +0000

    pushing up again after 7 had some goofy errors

--- 0x07-recursion/7-is_palindrome.c
+++ 0x07-recursion/7-is_palindrome.c
@@ -1,4 +1,15 @@
-
+#include <stdio.h>
+/**
+ * _strlen_recursion - returns the length of the string
+ * @s: string to find the length of.
+ * Return: (Success)
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s != '\0')
+		return (1 + _strlen_recursion(s + 1));
+	return (0);
+}
 /**
  * pal_checker - helper function to check if palindrome
  * @str
@@ -9,12 +20,13 @@
 
 int pal_checker(char str[], int start, int end)
 {
-	if (start == end)
+	/* printf("start %d end %d \n", start, end);*/
+	if (start >= end)
 		return (1);
 	if (str[start] != str[end])
 		return (0);
-	if (start <= end)
-		return pal_checker(str, start + 1, end - 1);
+	if (start <= end || start < end + 1)
+		return (pal_checker(str, start + 1, end - 1));
 	return (1);
 }
 
@@ -28,8 +40,8 @@ int is_palindrome(char *s)
 {
 	int length;
 
-	while(s[length] != '\0')
-		length = length + 1;
+	length = _strlen_recursion(s);
+
 	if (length == 0)
 		return (1);
 	return (pal_checker(s, 0, length - 1));

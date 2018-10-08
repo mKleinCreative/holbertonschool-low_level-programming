ab80fa217b64380e9cd4e6995738559854970abe
mKleinCreative <miketheklein@gmail.com>
2018-10-08 02:53:19 +0000

    fixed issue with 1

--- 0x09-argc_argv/1-args.c
+++ 0x09-argc_argv/1-args.c
@@ -2,13 +2,15 @@
 #include <stdio.h>
 /**
  * main - prints out number of args passed to function followed by new line
+ * @argv: arguements passed in.
  * @argc: number or arguments passed
  * Return: (Success)
  */
 
 int main(int argc, char *argv[])
 {
-	printf("%d\n", argc);
+	if (*argv)
+		printf("%d\n", argc - 1);
 	return (0);
 }
 

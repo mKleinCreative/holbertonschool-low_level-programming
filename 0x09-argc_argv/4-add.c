0e2cae7db1ce99ff483a455740275ba098966987
mKleinCreative <miketheklein@gmail.com>
2018-10-08 15:56:22 +0000

    fixed error of not returning the right error case

--- 0x09-argc_argv/4-add.c
+++ 0x09-argc_argv/4-add.c
@@ -14,7 +14,10 @@ int main(int argc, char *argv[])
 
 	sum = 0;
 	if (argc == 1)
-		return (printf("%d\n", 0));
+	{
+		printf("%d\n", 0);
+		return (0);
+	}
 	for (i = 1; i < argc; i++)
 	{
 		for (j = 0; argv[i][j] != '\0'; j++)

5810fa11f3cdccc41b41369ee7f9bc2668f7612b
mKleinCreative <miketheklein@gmail.com>
2018-10-08 03:43:52 +0000

    pushing 4 for testing

--- 0x09-argc_argv/4-add.c
+++ 0x09-argc_argv/4-add.c
@@ -2,27 +2,29 @@
 #include <stdlib.h>
 
 /**
- * main - Entry point
- *
+ * main - parses and adds all command args that are ints
+ * @argc: number of arguements
+ * @argv: array of ints to add
  * Return: (Success)
  */
 
-int main(int argc, char argv[])
+int main(int argc, char *argv[])
 {
 	int sum, i;
 
 	sum = 0;
-	if (argc <= 1)
-		return printf("%d\n", 0);
-	for (i = argc - 1; i >= 0; i--)
+	if (argc == 1)
+		return (printf("%d\n", 0));
+	for (i = 0; i < argc; i++)
 	{
-		if (argv[i] >= 30 && argv[i] <= 39)
-			sum += atoi(argv[i]) + sum;
-		else
+		if (*(*(argv) + i) < 30 || *(*(argv) + i) > 39)
 		{
 			printf("Error\n");
+			return (0);
 		}
+		sum += atoi(argv[i]);
 	}
+	printf("sum is %d\n", sum);
 	return (0);
 }
 

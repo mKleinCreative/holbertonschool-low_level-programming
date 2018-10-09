dbefa541a81b695aca32e9f02da27a0eb97ce893
mKleinCreative <miketheklein@gmail.com>
2018-10-09 23:58:56 +0000

    added null operator on last char for new array edge case

--- 0x0A-malloc_free/2-str_concat.c
+++ 0x0A-malloc_free/2-str_concat.c
@@ -12,14 +12,10 @@ char *str_concat(char *s1, char *s2)
 	int i, s1_len, s2_len;
 	char *temp;
 
-	if (s1 == NULL)
+	if (s1 == NULL || s2 == NULL)
 		return (NULL);
-	if (s2 == NULL)
-		return (NULL);
-
 	for (s1_len = 0; s1[s1_len]; s1_len++)
 		;
-
 	for (s2_len = 0; s2[s2_len]; s2_len++)
 		;
 	temp = malloc(sizeof(char) * (s1_len + s2_len + 1));
@@ -27,5 +23,6 @@ char *str_concat(char *s1, char *s2)
 		temp[i] = s1[i];
 	for (i = 0; i < s2_len; i++)
 		temp[s1_len + i] = s2[i];
+	temp[i] = '\0';
 	return (temp);
 }

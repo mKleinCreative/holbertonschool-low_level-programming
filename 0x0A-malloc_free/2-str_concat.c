9769b4fde129034d830837da324c4c028f6b5db4
mKleinCreative <miketheklein@gmail.com>
2018-10-09 23:45:50 +0000

    testing 2

--- 0x0A-malloc_free/2-str_concat.c
+++ 0x0A-malloc_free/2-str_concat.c
@@ -10,6 +10,19 @@
 char *str_concat(char *s1, char *s2)
 {
 	int i, s1_len, s2_len;
+	char *temp;
 
-	return (0);
+	if (s1 == NULL)
+		return (NULL);
+	for (s1_len = 0; s1[s1_len]; s1_len++)
+		;
+
+	for (s2_len = 0; s2[s2_len]; s2_len++)
+		;
+	temp = malloc(sizeof(char) * (s1_len + s2_len + 1));
+	for (i = 0; i < s1_len; i++)
+		temp[i] = s1[i];
+	for (i = 0; i < s2_len; i++)
+		temp[s1_len + i] = s2[i];
+	return (temp);
 }

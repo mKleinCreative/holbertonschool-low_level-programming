fbe27df62f0f7a61d8f9e46439e9ab7062c3e2a3
mKleinCreative <miketheklein@gmail.com>
2018-10-10 23:45:24 +0000

    pushing for testing

---
+++ 0x0A-malloc_free/100-strtow.c
@@ -0,0 +1,52 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * strtow - splits a string into separate words
+ * @str: string to split.
+ * Return: null if string is empty or fail, otherwise pointer to new arr
+ */
+
+char **strtow(char *str)
+{
+	int index, word_find, curr_word, letter_count, word_count, letter;
+	char **words;
+
+	if (!str)
+		return (NULL);
+	for (index = 0; str[index]; index++)
+	{
+		if (index != ' ' && (str[index + 1] == ' ' || str[index + 1] == '\0'))
+			word_count++;
+	}
+	words = malloc(sizeof(char *) * (word_count + 1));
+	if (!words)
+		return (NULL);
+	curr_word = 0;
+	for (word_find = 0; str[word_find]; word_find++)
+	{
+		letter_count = 0;
+		if (str[word_find] != ' ')
+		{
+			for (index = word_find; str[index] != '\0'; index++)
+			{
+				if (str[index] == ' ')
+					break;
+				letter_count++;
+			}
+			words[curr_word] = malloc(sizeof(char) * (letter_count + 1));
+			if (!words[curr_word])
+			{
+				for (; curr_word >= 0; curr_word--)
+					free(words[curr_word]);
+				free(words);
+			}
+			for (letter = 0; word_find < index; word_find++)
+				words[curr_word][letter++] = str[word_find];
+			curr_word++;
+		}
+	}
+	words[curr_word] = NULL;
+	return (words);
+}

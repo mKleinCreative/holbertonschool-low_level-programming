3b719677fa7e6f6ac4dce49adedd63322d495fdd
mKleinCreative <miketheklein@gmail.com>
2018-10-10 01:31:54 +0000

    Testing 5

--- 0x0A-malloc_free/5-argstostr.c
+++ 0x0A-malloc_free/5-argstostr.c
@@ -2,13 +2,27 @@
 #include <stdlib.h>
 
 /**
- * main - Entry point
- *
- * Return: (Success)
+ * argstostr - concatenates all args of program
+ * @ac: count of args
+ * @av: values of args
+ * Return: each argument followed by \n
  */
 
-int main(void)
+char *argstostr(int ac, char **av)
 {
+	char *tmp;
+	int i, length;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	tmp = malloc(sizeof(char) * ac);
+	if (tmp == NULL)
+		return (NULL);
+	for (i = 0; i < ac; i++)
+	{
+		for (length = 0; length < ac; length++)
+			tmp[length + i] = av[i][length];
+	}
 	return (0);
 }
 

a871ae38c812cf6fde103caa5ab784a76386fe26
mKleinCreative <miketheklein@gmail.com>
2018-10-10 00:48:19 +0000

    pushing up 3 for testing

--- 0x0A-malloc_free/3-alloc_grid.c
+++ 0x0A-malloc_free/3-alloc_grid.c
@@ -2,13 +2,31 @@
 #include <stdlib.h>
 
 /**
- * main - Entry point
- *
- * Return: (Success)
+ * alloc_grid - returns a poniter to 2d array of ints
+ * @width: width of array.
+ * @height: height of array.
+ * Return: the printed array or null on failure.
  */
 
-int main(void)
+int **alloc_grid(int width, int height)
 {
-	return (0);
+	int **grid;
+	int i, j;
+
+	grid = malloc(height * sizeof(*grid));
+
+	for (i = 0; i < height; i++)
+	{
+		grid[i] = (int *)malloc(width * sizeof(int));
+		if (grid[i] == NULL)
+		{
+			for (; i >= 0; i--)
+				free(grid[i]);
+			free(grid);
+		}
+		for (j = 0; j < width; j++)
+			grid[i][j] = 0;
+	}
+	return (grid);
 }
 

3af72c7f51c6fda92dd2ec79b696b13749bd3660
mKleinCreative <miketheklein@gmail.com>
2018-10-11 21:06:20 +0000

    pushing 3 for testing

--- 0x0B-more_malloc_free/3-array_range.c
+++ 0x0B-more_malloc_free/3-array_range.c
@@ -2,13 +2,25 @@
 #include <stdlib.h>
 
 /**
- * - Entry point
- *
- * Return: (Success)
+ * array_range - creates an array of integers.
+ * @min: smallest int.
+ * @max: largest int.
+ * Return: pointer to array of all values from min to max
  */
 
-int main(void)
+int *array_range(int min, int max)
 {
-	return (0);
+	int i, range,*arr;
+
+	if (min > max)
+		return (NULL);
+	range = max - min;
+	arr = malloc(sizeof(int) * range + 1);
+	if (arr == NULL)
+		return (NULL);
+	for (i = 0; i < range; i++)
+		arr[i] = i + min;
+	arr[i] = max;
+	return (arr);
 }
 

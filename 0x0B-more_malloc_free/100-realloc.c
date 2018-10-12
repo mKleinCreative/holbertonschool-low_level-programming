985e577d5fb3246d68725fa063a6160b6cffc41f
mKleinCreative <miketheklein@gmail.com>
2018-10-12 21:18:46 +0000

    worked through realloc

---
+++ 0x0B-more_malloc_free/100-realloc.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _memcpy - copies a space in memory
+ * @dest: destination to copy too.
+ * @src: data to copy
+ * @n: how much data to copy.
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+	while (n--)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
+}
+
+
+/**
+ * _realloc - creates an array of integers.
+ * @ptr: memory previously allocated.
+ * @old_size: old size of array.
+ * @new_size: new size of memory block.
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	void *new;
+	if (new_size == old_size)
+		return (ptr);
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	new = malloc(new_size);
+	if (new == NULL)
+		return (NULL);
+	if (new_size > old_size)
+	{
+		free(ptr);
+		return (new);
+		_memcpy (new, ptr, old_size);
+		free(ptr);
+	}
+	return (new);
+}

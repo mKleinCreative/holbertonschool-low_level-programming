034ff41c9b649a81c46c596e4920250b5d818bd1
mKleinCreative <miketheklein@gmail.com>
2018-10-12 21:22:06 +0000

    fixed betty errors for 100

--- 0x0B-more_malloc_free/100-realloc.c
+++ 0x0B-more_malloc_free/100-realloc.c
@@ -6,11 +6,13 @@
  * @dest: destination to copy too.
  * @src: data to copy
  * @n: how much data to copy.
+ * Return: returns pointer to new array
  */
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
+
 	while (n--)
 	{
 		dest[i] = src[i];
@@ -25,10 +27,12 @@ char *_memcpy(char *dest, char *src, unsigned int n)
  * @ptr: memory previously allocated.
  * @old_size: old size of array.
  * @new_size: new size of memory block.
+ * Return: nothing.
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	void *new;
+
 	if (new_size == old_size)
 		return (ptr);
 	if (new_size == 0 && ptr != NULL)
@@ -44,7 +48,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	{
 		free(ptr);
 		return (new);
-		_memcpy (new, ptr, old_size);
+		_memcpy(new, ptr, old_size);
 		free(ptr);
 	}
 	return (new);

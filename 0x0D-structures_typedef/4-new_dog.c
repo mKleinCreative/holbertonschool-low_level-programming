498c53f4b701f0215f3ca1615b63417c41906bfb
mKleinCreative <miketheklein@gmail.com>
2018-10-16 05:10:19 +0000

    testing edgecases for 4

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -13,7 +13,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	struct dog *d;
 
 	d = malloc(sizeof(d));
-	if (d == NULL)
+	if (d == NULL || !name || !owner)
 		return (NULL);
 	d->name = name;
 	d->age = age;

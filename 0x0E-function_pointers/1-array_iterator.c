a63bd42430ce9b92560f13989a356d576734be35
mKleinCreative <miketheklein@gmail.com>
2018-10-17 19:46:58 +0000

    pushing up 1 and 2 for testing

--- 0x0E-function_pointers/1-array_iterator.c
+++ 0x0E-function_pointers/1-array_iterator.c
@@ -1,3 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
-array_iterator.c
+
+/**
+ *  array_iterator - executes a function on a given element of an array
+ * @array: array to iterate over.
+ * @size: size of array to iterate over
+ * @action: function to apply to array
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	if (!size || !array || !action)
+		return;
+	for (; size; size--)
+		action(*array++);
+}
--- 0x0E-function_pointers/2-int_index.c
+++ 0x0E-function_pointers/2-int_index.c
@@ -1,3 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
-int_index.c
+
+/**
+ * int_index - executes a function on a given element of an array
+ * @array: array to iterate over.
+ * @size: size of array to iterate over
+ * @cmp: function that compares values.
+ */
+
+int int_index(int *array, size_t size, int (*cmp)(int))
+{
+	int temp;
+
+	temp = size;
+	if (!array)
+		return (0);
+	if (size <= 0)
+		return (-1);
+	for (; size; size--)
+		if (cmp(array[temp - size]))
+			return (temp - size);
+	return (-1);
+}

1205891a42419d716c929656caad211cf08e6efe
mKleinCreative <miketheklein@gmail.com>
2018-10-18 02:06:02 +0000

    testing multiple operator edgecase

--- 0x0E-function_pointers/3-get_op_func.c
+++ 0x0E-function_pointers/3-get_op_func.c
@@ -20,7 +20,7 @@ int (*get_op_func(char *s))(int, int)
 	int i;
 
 	i = 0;
-	while (ops[i].op != NULL && ops[i].op[1] == '\0')
+	while (ops[i].op != NULL && s[1] == '\0')
 	{
 		if (ops[i].op[0] == s[0])
 			return (ops[i].f);

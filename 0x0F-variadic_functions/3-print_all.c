a9751220b83844e4ad8fa2bce90f0f42ba25edad
mKleinCreative <miketheklein@gmail.com>
2018-10-22 03:49:56 +0000

    fixed edge cases on 3

--- 0x0F-variadic_functions/3-print_all.c
+++ 0x0F-variadic_functions/3-print_all.c
@@ -1,24 +1,14 @@
 #include "variadic_functions.h"
 
 /**
- * struct format - finds the format of info
- * @flag: flag given to the format struct.
- * @f: call back function for the struct.
- */
-typedef struct format
-{
-	char *flag;
-	int (*f)(va_list);
-} formatter;
-
-/**
  * print_char - prints a char
  * @a: char to print
  */
 void print_char(va_list a)
 {
-	printf("%c", va(arg(a, int));
+	printf("%c", va_arg(a, int));
 }
+
 /**
  * print_int - prints an int
  * @a: int to print
@@ -27,6 +17,7 @@ void print_int(va_list a)
 {
 	printf("%d", va_arg(a, int));
 }
+
 /**
  * print_float - prints a float
  * @a: float to print
@@ -35,6 +26,7 @@ void print_float(va_list a)
 {
 	printf("%f", va_arg(a, double));
 }
+
 /**
  * print_char - prints a char
  * @a: char to print
@@ -42,41 +34,46 @@ void print_float(va_list a)
 void print_string(va_list a)
 {
 	char *current;
-	current = va_arg(val, char *);
-	if (!current)
+
+	current = va_arg(a, char *);
+	if (current == NULL)
+	{
 		printf("(nil)");
-	printf("%s", a);
+		return;
+	}
+	else
+		printf("%s", current);
 }
+
 /**
  * print_all - prints all args passed to function
  * @format: list of args to give data types.
  */
-
 void print_all(const char * const format, ...)
 {
 	va_list list;
+	const char *copy;
 	formatter formats[] = {
-		{"c", printf("%c", print_char)},
-		{"i", printf("%d", print_int)},
-		{"f", printf("%f", print_float)},
-		{"s", printf("%c", print_string)},
+		{"c", print_char},
+		{"i", print_int},
+		{"f", print_float},
+		{"s", print_string},
 		{NULL, NULL}
 	};
 	int i, j;
 
-	j = 0;
-	if (format == NULL)
-		return;
+	i = j = 0;
+	copy = format;
 	va_start(list, format);
-	while(format && format[j])
+	while(copy && copy[j] != '\0')
 	{
-		i = 0
+		i = 0;
 		while (formats[i].flag)
 		{
-			if (formats[i].flag[0] == format[0])
+			if (formats[i].flag[0] == copy[j])
 			{
-				(formats[i].f)(va_arg(list, void *);
-				 if (format[j + 1])
+				(formats[i].f)(list);
+				 if (copy[j + 1])
 				 	printf(", ");
 			}
 			i++;

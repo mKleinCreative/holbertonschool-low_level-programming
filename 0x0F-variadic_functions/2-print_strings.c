bc38bc5c795d6043cfead5c7aa433d8d690ec65d
mKleinCreative <miketheklein@gmail.com>
2018-10-22 02:50:19 +0000

    checking edge cases for 2-print strings

--- 0x0F-variadic_functions/2-print_strings.c
+++ 0x0F-variadic_functions/2-print_strings.c
@@ -10,20 +10,23 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	va_list string_list;
 	unsigned int i;
 
-	if (separator == NULL)
-		return;
 	va_start(string_list, n);
 
-	for (i = 0; i < n - 1; i++)
+	for (i = 0; i < n;)
 	{
-		if (va_arg(string_list, const char *) == NULL)
+		item = va_arg(string_list, char*)
+		if (item == NULL)
 		{
-			printf("(nil)%s", separator);
+			printf("(nil)");
 		} else
 		{
-			printf("%s%s", va_arg(string_list, const char *), separator);
+			printf("%s%s", item, separator);
 		}
+		i++;
+		if (i < n && separator)
+			printf("%s", separator);
+			if (i == n - 1)
+				printf("\n");
 	}
-	printf("%s\n", va_arg(string_list, const char *));
 	va_end(string_list);
 }

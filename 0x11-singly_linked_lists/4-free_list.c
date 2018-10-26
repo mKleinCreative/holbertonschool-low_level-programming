c241cb4855a1b1d6ebd75f607d3b97237187a2df
mKleinCreative <miketheklein@gmail.com>
2018-10-26 20:03:30 +0000

    testing add node end

--- 0x11-singly_linked_lists/4-free_list.c
+++ 0x11-singly_linked_lists/4-free_list.c
@@ -1,3 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
-free_list.c
+
+/**
+ * free_list - frees up a node in a linked list
+ * @head: head of the linked list 
+ */
+
+void free_list(list_t *head)
+{
+	list_t *free_node;
+
+	while (head)
+	{
+		free_node = head;
+		head = head->next;
+		free(free_node->str);
+		free(free_node);
+	}
+}

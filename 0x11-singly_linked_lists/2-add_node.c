9466cf45e8f0efe599b92941101757c102badb66
mKleinCreative <miketheklein@gmail.com>
2018-10-26 15:31:23 +0000

    setting up betty errors

--- 0x11-singly_linked_lists/2-add_node.c
+++ 0x11-singly_linked_lists/2-add_node.c
@@ -1,3 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
-add_node.c
+#include "lists.h"
+/**
+ * add_node - adds a node to the linked list
+ * @h: pointer to head of linked list to find the size of
+ * Return: number of nodes in the list
+ */
+size_t *add_node(const list_t **head, const char *str)
+{
+	int count = 0;
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+
+	return (count);
+}

154e89fdcd267f33d9e838e761760edd74dee75c
mKleinCreative <miketheklein@gmail.com>
2018-10-26 01:37:58 +0000

    settled 0 edge case

--- 0x11-singly_linked_lists/0-print_list.c
+++ 0x11-singly_linked_lists/0-print_list.c
@@ -9,11 +9,20 @@
 size_t print_list(const list_t *h)
 {
 	int count = 0;
+
 	while (h != NULL)
 	{
-		printf("[%d] %s\n", h-> len, h->str);
-		count++;
-		h = h->next;
+		if (h->str == NULL)
+		{
+			printf("[%d] %s\n", 0, "(nil)");
+			count++;
+			h = h->next;
+		} else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+			count++;
+			h = h->next;
+		}
 	}
 
 	return (count);

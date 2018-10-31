90b7b6667a688b4527795d809de441d29c025afb
mKleinCreative <miketheklein@gmail.com>
2018-10-31 19:37:10 +0000

    testing delete

--- 0x12-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x12-more_singly_linked_lists/10-delete_nodeint.c
@@ -24,7 +24,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		free(delete);
 		return (1);
 	}
-	for (i = 0; temp != '\0'; i++)
+	for (i = 0; temp != NULL; i++)
 	{
 		if (i + 1 == index)
 			break;
@@ -35,6 +35,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	{
 		delete = temp->next;
 		temp->next = temp->next->next;
+		free(delete);
 	}
 	return (1);
 }

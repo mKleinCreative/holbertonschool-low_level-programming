093c6d9a1617112c9acb94853bd709590bd65519
mKleinCreative <miketheklein@gmail.com>
2018-10-31 17:31:37 +0000

    testing fixes for 5 and 6

--- 0x12-more_singly_linked_lists/6-pop_listint.c
+++ 0x12-more_singly_linked_lists/6-pop_listint.c
@@ -14,12 +14,11 @@ int pop_listint(listint_t **head)
 	listint_t *tmp;
 	int node_data;
 
-	if (head == NULL)
+	if (head == NULL || !*head)
 		return (0);
 	tmp = *head;
-	if (tmp == NULL)
-		return (0);
 	node_data = tmp->n;
-	**head = *tmp->next;
+	*head = *tmp->next;
+	free(tmp);
 	return (node_data);
 }

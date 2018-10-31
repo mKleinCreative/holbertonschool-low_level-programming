57c0e058c327da062b274d9a5368a2dbf3254559
mKleinCreative <miketheklein@gmail.com>
2018-10-31 17:46:00 +0000

    pushing 7 for testing

--- 0x12-more_singly_linked_lists/7-get_nodeint.c
+++ 0x12-more_singly_linked_lists/7-get_nodeint.c
@@ -18,7 +18,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	if (!head)
 		return (NULL);
 	temp = head;
-	for (count = 0; count <= index; count++)
+	for (count = 0; head; count++)
 	{
 		if (count == index)
 			return (temp);

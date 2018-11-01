bb40d26fb110d01de6c24afad12dd39c0168f173
mKleinCreative <miketheklein@gmail.com>
2018-11-01 23:29:54 +0000

    pushing 1 for testing

--- 0x13-bit_manipulation/1-print_binary.c
+++ 0x13-bit_manipulation/1-print_binary.c
@@ -1,14 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "holberton.h"
 
 /**
- * print_binary.c - Entry point
- *
- * Return: (Success)
+ * print_binary - prints the binary representation of a number.
+ * @n: number to print
+ * Return: number converted to binary
  */
 
-int main(void)
+void print_binary(unsigned long int n)
 {
-	return (0);
+	unsigned int i = 0;
+	int flag = 0;
+	unsigned int highestOne = 1 << (sizeof(unsigned int)*8 - 1);
+
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+	for (i = 0; i < sizeof(unsigned int) * 8; i++)
+	{
+		if ((n & (highestOne >> i)) != 0)
+			flag = 1;
+		if (flag)
+			_putchar(((n & (highestOne >> i)) != 0) + '0');
+	}
 }
 

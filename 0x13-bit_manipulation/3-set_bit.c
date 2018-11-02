a2af09900ce09af3e3f8c8e25bc15227cc941193
mKleinCreative <miketheklein@gmail.com>
2018-11-02 01:31:59 +0000

    checking 3 for edgecases, am now returning right thing (hopefully)

--- 0x13-bit_manipulation/3-set_bit.c
+++ 0x13-bit_manipulation/3-set_bit.c
@@ -11,7 +11,10 @@
 int set_bit(unsigned long int *n, unsigned int index)
 {
 	if (n && index)
-		return (*n = *n | 1UL << index);
+	{
+		*n = *n | 1UL << index;
+		return (1);
+	}
 	else
 		return (-1);
 	return (-1);

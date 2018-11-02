0f8b3720c01c3f4517ef5b4f96131c7e1b7bbd1b
mKleinCreative <miketheklein@gmail.com>
2018-11-02 21:29:11 +0000

    pushing 4 for testing

--- 0x13-bit_manipulation/4-clear_bit.c
+++ 0x13-bit_manipulation/4-clear_bit.c
@@ -2,13 +2,22 @@
 #include <stdlib.h>
 
 /**
- * clear_bit.c - Entry point
- *
- * Return: (Success)
+ * clear_bit - sets the value of a bit to 0 at a given index
+ * @n: number to iterate over
+ * @index: index to clear at
+ * Return: 1 if worked or -1 if there was an error
  */
 
-int main(void)
+int clear_bit(unsigned long int *n, unsigned int index)
 {
-	return (0);
+	int bit;
+
+	if (index > 31)
+		return (-1);
+	bit = ~(1 << index);
+
+	*n = *n & bit;
+
+	return (1);
 }
 

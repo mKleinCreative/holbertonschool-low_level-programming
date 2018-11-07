0f9a30011753f8dd22b45f985318f4fcbdd45341
mKleinCreative <miketheklein@gmail.com>
2018-11-07 03:33:00 +0000

    testing edge cases on 2

--- 0x14-file_io/2-append_text_to_file.c
+++ 0x14-file_io/2-append_text_to_file.c
@@ -13,7 +13,7 @@ int append_text_to_file(const char *filename, char *text_content)
 {
 	int length, file, size;
 
-	if (filename == NULL)
+	if (!filename || !text_content)
 		return (-1);
 	file = open(filename, O_WRONLY | O_APPEND);
 	if (file < 0)

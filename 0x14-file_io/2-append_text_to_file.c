da1fe379f16d2776c01890888bbe9af39076a6d7
mKleinCreative <miketheklein@gmail.com>
2018-11-07 03:27:55 +0000

    testing 2

--- 0x14-file_io/2-append_text_to_file.c
+++ 0x14-file_io/2-append_text_to_file.c
@@ -1,14 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "holberton.h"
 
 /**
- * append_text_to_file.c - Entry point
- *
- * Return: (Success)
+ * append_text_to_file - append text to the end of a file
+ * @filename: name of file to create
+ * @text_content: content to append to the file
+ * Return: 1 if successful, -1 if it failed
  */
 
-int main(void)
+int append_text_to_file(const char *filename, char *text_content)
 {
-	return (0);
+	int length, file, size;
+
+	if (filename == NULL)
+		return (-1);
+	file = open(filename, O_WRONLY | O_APPEND);
+	if (file < 0)
+		return (-1);
+	for (length = 0; text_content[length];)
+		length++;
+	size = write(file, text_content, length);
+	if (size < 0)
+		return (-1);
+	close(file);
+	return (1);
 }
 

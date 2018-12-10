31c72727c70ff8507c252af2c98504a16e486cda
mKleinCreative <miketheklein@gmail.com>
2018-12-10 19:18:17 +0000

    Pushing 5 for testing

--- 0x16-doubly_linked_lists/5-get_dnodeint.c
+++ 0x16-doubly_linked_lists/5-get_dnodeint.c
@@ -1,14 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "lists.h"
 
 /**
  * get_dnodeint.c - Entry point
- *
- * Return: (Success)
+ * @head: head of the DLL to find node in.
+ * @index: index of where the node should be.
+ * Return: index of where the node is, or NULL if it doesn't exist.
  */
 
-int main(void)
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-	return (0);
+	unsigned int current;
+	dlistint_t *node;
+
+	node = head;
+	current = 0;
+
+	while (node)
+	{
+		if (current == index)
+			return (node);
+		if (node->next == NULL && index > current + 1)
+			return (NULL);
+		else
+		{
+			current++;
+			node = node->next;
+		}
+	}
+	return (NULL);
 }
 

4a13d49948543d7e1353fc1204b38a877c926e22
mKleinCreative <miketheklein@gmail.com>
2018-12-10 18:16:06 +0000

    pushing up 3 for testing

--- 0x16-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x16-doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,14 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "lists.h"
 
 /**
- * add_dnodeint_end.c - Entry point
- *
- * Return: (Success)
+ * add_dnodeint_end - inserts a node at the end of the DLL
+ * @head: head of list to append node too.
+ * @n: value of new node to add to list.
+ * Return: address of new element or NULL if failed.
  */
 
-int main(void)
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-	return (0);
+	dlistint_t *new;
+	dlistint_t *last = *head;
+
+	new = (malloc(sizeof(dlistint_t)));
+	if (!new)
+		return (NULL);
+	if (*head == NULL)
+	{
+		new->next = NULL;
+		new->prev = NULL;
+		new->n = n;
+		*head = new;
+		return (*head);
+	}
+	do {
+		if (last->next == NULL)
+		{
+			last->next = new;
+			new->next = NULL;
+			new->prev = last;
+			new->n = n;
+			return (*head);
+		}
+		last = last->next;
+	} while (last);
+	free(new);
+	return (*head);
 }
 

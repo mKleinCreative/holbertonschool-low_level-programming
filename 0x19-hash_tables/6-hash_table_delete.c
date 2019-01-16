f3bc5beea6b6d98adbdfdca7ca4d93724d73c5ec
mKleinCreative <miketheklein@gmail.com>
2019-01-16 03:30:17 +0000

    pushing up deletion for testing

--- 0x19-hash_tables/6-hash_table_delete.c
+++ 0x19-hash_tables/6-hash_table_delete.c
@@ -2,13 +2,30 @@
 #include <stdlib.h>
 
 /**
- * hash_table_delete.c - Entry point
- *
- * Return: (Success)
+ * hash_table_delete - deletes a hash table
+ * @ht: head of hash table to delete
  */
 
-int main(void)
+void hash_table_delete(hash_table_t *ht)
 {
-	return (0);
+	hash_node_t *del_me, *del_me_next;
+	unsigned long int index;
+
+	if (!ht)
+		return;
+	for (index = 0; index < ht->size; index++)
+	{
+		del_me = ht->array[index];
+		while(del_me)
+		{
+			del_me_next = del_me->next;
+			free(del_me->key);
+			free(del_me->value);
+			free(del_me);
+			del_me = del_me_next;
+		}
+	}
+	free(ht->array);
+	free(ht);
 }
 

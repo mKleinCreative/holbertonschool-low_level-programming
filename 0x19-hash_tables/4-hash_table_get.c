d89623587230507a07d21f4fc92a0bd2421ccade
mKleinCreative <miketheklein@gmail.com>
2019-01-16 01:41:20 +0000

    pushing up 4 for testing

--- 0x19-hash_tables/4-hash_table_get.c
+++ 0x19-hash_tables/4-hash_table_get.c
@@ -12,11 +12,11 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int i;
 	hash_node_t *bucket_head;
 
-	if (ht->size == 0 || ht == NULL)
+	if (ht == NULL || ht->size == 0)
 		return (NULL);
 	i = key_index((unsigned char *)key, ht->size);
 	bucket_head = ht->array[i];
-	for (bucket_head = ht->array[i]; bucket_head; bucket_head = bucket_head->next)
+	for (bucket_head = ht->array[i]; bucket_head != NULL; bucket_head = bucket_head->next)
 	{
 		if (strcmp(bucket_head->key, key) == 0)
 			return (bucket_head->value);
